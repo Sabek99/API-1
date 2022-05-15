2760b5f9273a18938f1d15cc0d954bf37e070ba4
Mohamed Sabek <mmsabek99@gmail.com>
2022-05-15 14:19:58 +0200

    Question Pagination and Question service updates

---
+++ WebApi/Models/Pagination/PaginationParams.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace WebApi.Models.Pagination;
+
+public class PaginationParams
+{
+    private int itemsPerPage;
+    private const int _maxItemsPerPage = 50;
+    public int Page { get; set; } = 1;
+
+    public int ItemPerPage
+    {
+        get => itemsPerPage;
+        set => itemsPerPage = value > _maxItemsPerPage ? _maxItemsPerPage : value;
+    }
+}
