e1ad49d363ba73bcce108b24be39389f6a65f414
NourNafea <nour68.nafea@gmail.com>
2022-05-21 17:13:10 +0200

    Add roles

--- WebApi/Authorization/AuthorizeAttribute.cs
+++ WebApi/Authorization/AuthorizeAttribute.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Identity;
-
 namespace WebApi.Authorization;
 
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +7,13 @@ using WebApi.Entities;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 {
+    private readonly IList<Role> _roles;
+
+    public AuthorizeAttribute(params Role[] roles)
+    {
+        _roles = roles ?? new Role[] { };
+    }
+
     public void OnAuthorization(AuthorizationFilterContext context)
     {
         // skip authorization if action is decorated with [AllowAnonymous] attribute
@@ -18,8 +23,10 @@ public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 
         // authorization
         var user = (User)context.HttpContext.Items["User"];
-
-        if (user == null || user.Token == null)
+        if (user?.Token == null || (_roles.Any() && !_roles.Contains(user.Role)))
+        {
+            // not logged in or role not authorized
             context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
     }
 }
